06e6d6544d156699efee1241c215d7e2fcf4bbfd
Thiagarajan Baskarasubramanian <itthiagu@gmail.com>
2026-03-07 17:13:38 +0530

    Commit for code review from a collaborator

--- app.py
+++ app.py
@@ -17,10 +17,12 @@ if __name__ == "__main__":
     num2 = 4
     num3 = 25
     num4 = 144
+    num5 = 1000
     print(f"{num1} + {num2} = {calculator.add(num1, num2)}")
     print(f"{num1} - {num2} = {calculator.subtract(num1, num2)}") 
     print(f"{num1} * {num2} = {calculator.multiply(num1, num2)}")
     print(f"{num1} / {num2} = {calculator.divide(num1, num2)}")
     print(f"√{num3} = {calculator.square_root(num3)}")
     print(f"√{num4} = {calculator.square_root(num4)}")
+    print(f"√{num5} = {calculator.square_root(num5)}")
 
