108a70b86941364f88b6e57edb1b4f401955c044
Thiagarajan Baskarasubramanian <itthiagu@gmail.com>
2026-03-10 23:30:12 +0530

    Add circle area feature

--- app.py
+++ app.py
@@ -1,31 +1,12 @@
 import math
-class Calculator:
-    def add(self, a, b):
-        return a + b
-    def subtract(self, a, b):
-        return a - b
-    def multiply(self, a, b):
-        return a * b
-    def divide(self, a, b):
-        if b == 0:
-            raise ValueError("Cannot divide by zero.")
-        return a / b
-    def square_root(self, x):
-        return math.sqrt(x)
+class GeometryCalculator:
+	def calculate_circle_area(self, radius):
+		return math.pi * radius ** 2
+	def calculate_rectangle_area(self, length, width):
+		return length * width
 
 if __name__ == "__main__":
-    calculator = Calculator()
-    num1 = 16
-    num2 = 4
-    num3 = 25
-    num4 = 144
-    num5 = 1000
-    
-    print(f"{num1} + {num2} = {calculator.add(num1, num2)}")
-    print(f"{num1} - {num2} = {calculator.subtract(num1, num2)}") 
-    print(f"{num1} * {num2} = {calculator.multiply(num1, num2)}")
-    print(f"{num1} / {num2} = {calculator.divide(num1, num2)}")
-    print(f"√{num3} = {calculator.square_root(num3)}")
-    print(f"√{num4} = {calculator.square_root(num4)}")
-    print(f"√{num5} = {calculator.square_root(num5)}")
-
+	calculator = GeometryCalculator()
+	
+radius = 5
+print(f"The area of the circle with radius {radius} = {calculator.calculate_circle_area(radius)}")
