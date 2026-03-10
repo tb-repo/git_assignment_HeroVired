f934524288d8b82a5b45d8f48ae911789755ec82
Thiagarajan Baskarasubramanian <itthiagu@gmail.com>
2026-03-10 23:32:52 +0530

    Add rectangle area feature

--- app.py
+++ app.py
@@ -7,3 +7,7 @@ class GeometryCalculator:
 
 if __name__ == "__main__":
     calculator = GeometryCalculator()
+	
+length = 10
+width = 6
+print(f"The area of the rectangle with length {length} and width {width} = {calculator.calculate_rectangle_area(length,width)}")
